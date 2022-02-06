9f141a74cbf8faf399fe74819aa09e0e2514abfe
pdinsem2004 <pdinsem2004@yahoo.fr>
2022-02-06 14:14:25 +0100

    In the journey file each "unixTimestamp"  should correspond to  "customerId" and the "station"

--- src/test/java/dev/billing/BillingSystemTest.java
+++ src/test/java/dev/billing/BillingSystemTest.java
@@ -8,12 +8,14 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.rules.ExpectedException;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BillingSystemTest {
     @Rule
@@ -24,7 +26,6 @@ public class BillingSystemTest {
     private List<Price> priceList = new ArrayList<Price>();
     private List<Station> stationList = new ArrayList<Station>();
     private List<Zone> zoneList = new ArrayList<Zone>();
-
 
 
     @Before
@@ -43,7 +44,6 @@ public class BillingSystemTest {
      * zone should have a name, otherwise throw exception
      *
      * @throws RuntimeException
-     *
      */
     @Test
     public void theZoneNameIsNullOrEmptyThrowException() {
@@ -53,42 +53,42 @@ public class BillingSystemTest {
 
 
     }
+
     /**
      * Implementation of the Test case 1
      * If zone name is null or empty, throw exception
      *
      * @throws RuntimeException
-     *
      */
     public void theZoneNameIsNullOrEmptyThrowExceptionImpl(Zone zone) {
-        if (zone.getName() == "" || zone.getName()==null)
+        if (zone.getName() == "" || zone.getName() == null)
             throw new RuntimeException("Ivalid zone name");
     }
+
     /**
      * Test case 2
      * zone name is unique , otherwise throw exception
      *
      * @throws RuntimeException
-     *
      */
     @Test
     public void theZoneNameIsNotUniqueThrowException() {
         exception.expect(RuntimeException.class);
-        Zone zone1= new Zone(5, "zone1");
+        Zone zone1 = new Zone(5, "zone1");
         Zone zone2 = new Zone(6, "zone1");
         theZoneNameIsNotUniqueThrowExceptionImpl(zone1, zone2);
 
 
     }
+
     /**
      * Implementation of the Test case 2
      * zone name is unique , otherwise throw exception
      *
      * @throws RuntimeException
-     *
      */
     public void theZoneNameIsNotUniqueThrowExceptionImpl(Zone zone1, Zone zone2) {
-         if (zone1.getName().equalsIgnoreCase(zone2.getName()))
+        if (zone1.getName().equalsIgnoreCase(zone2.getName()))
             throw new RuntimeException("zone name must be unique");
     }
 
@@ -97,25 +97,24 @@ public class BillingSystemTest {
      * Zone contains at least one station , otherwise throw exception
      *
      * @throws RuntimeException
-     *
      */
     @Test
     public void zoneWithoutStationThrowException() {
         exception.expect(RuntimeException.class);
-        Zone zone1= new Zone(5, "zone1");
-       zoneWithoutStationThrowExceptionImpl(zone1);
+        Zone zone1 = new Zone(5, "zone1");
+        zoneWithoutStationThrowExceptionImpl(zone1);
 
 
     }
+
     /**
      * Implementation of the Test case 3
      * Zone contains at least one station, otherwise throw exception
      *
      * @throws RuntimeException
-     *
      */
     public void zoneWithoutStationThrowExceptionImpl(Zone zone) {
-       if (zone.getStations()==null || zone.getStations().size()==0)
+        if (zone.getStations() == null || zone.getStations().size() == 0)
             throw new RuntimeException("zone must contains station");
     }
 
@@ -124,83 +123,82 @@ public class BillingSystemTest {
      * In the pricing zoneFrom should be defined exist, if not throw an exception
      *
      * @throws RuntimeException
-     *
      */
     @Test
     public void zoneFromNotDefinedInTheJourneyThrowException() {
         exception.expect(RuntimeException.class);
-        Price price= new Price();
+        Price price = new Price();
         price.setPrice(priceList.get(0).getPrice());
         price.setZoneTo(zoneList.get(0));
         zoneFromNotDefinedInTheJourneyThrowExceptionImpl(price);
 
 
     }
+
     /**
      * Implementation of the Test case 4
      * In the pricing zoneFrom should be defined exist, if not throw an exception
      *
      * @throws RuntimeException
-     *
      */
     public void zoneFromNotDefinedInTheJourneyThrowExceptionImpl(Price price) {
-         if (price.getZoneFrom()==null)
+        if (price.getZoneFrom() == null)
             throw new RuntimeException("invade pricing rule ");
     }
+
     /**
      * Test case 5
      * In the pricing zoneTo should be defined and exist, if not throw an exception
      *
      * @throws RuntimeException
-     *
      */
     @Test
     public void zoneToNotDefinedInTheJourneyThrowException() {
         exception.expect(RuntimeException.class);
-        Price price= new Price();
+        Price price = new Price();
         price.setPrice(priceList.get(0).getPrice());
         price.setZoneFrom(zoneList.get(0));
         zoneToNotDefinedInTheJourneyThrowExceptionImpl(price);
 
 
     }
+
     /**
      * Implementation of the Test case 5
      * In the pricing zoneTo should be defined and exist, if not throw an exception
      *
      * @throws RuntimeException
-     *
      */
     public void zoneToNotDefinedInTheJourneyThrowExceptionImpl(Price price) {
-        if (price.getZoneTo()==null)
+        if (price.getZoneTo() == null)
             throw new RuntimeException("invade pricing rule ");
     }
+
     /**
      * Test case 6
      * In the pricing rule,  price should be not null and not equal to 0
      *
      * @throws RuntimeException
-     *
      */
     @Test
     public void priceIsNullOrZeroThrowException() {
         exception.expect(RuntimeException.class);
-        Price price= new Price();
+        Price price = new Price();
         price.setZoneFrom(zoneList.get(0));
         price.setZoneTo(zoneList.get(0));
         priceIsNullOrZeroThrowExceptionImpl(price);
 
 
     }
+
     /**
      * Implementation of the Test case 6
      * In the pricing rule,  price should be not null and not equal to 0
      *
      * @throws RuntimeException
-     *
      */
     public void priceIsNullOrZeroThrowExceptionImpl(Price price) {
-        if (price.getPrice()==0)
+        if (price.getPrice() == 0)
             throw new RuntimeException("invade pricing rule ");
     }
 
@@ -209,12 +207,11 @@ public class BillingSystemTest {
      * In the pricing rule,  price should be not positive value
      *
      * @throws RuntimeException
-     *
      */
     @Test
     public void priceIsNegativeThrowException() {
         exception.expect(RuntimeException.class);
-        Price price= new Price();
+        Price price = new Price();
         price.setZoneFrom(zoneList.get(0));
         price.setZoneTo(zoneList.get(0));
         price.setPrice(-10);
@@ -222,12 +219,12 @@ public class BillingSystemTest {
 
 
     }
+
     /**
      * Implementation of the Test case 6
      * In the pricing rule,  price should be not positive value
      *
      * @throws RuntimeException
-     *
      */
     public void priceIsNegativeThrowExceptionImpl(Price price) {
         if (price.getPrice() < 0)
@@ -239,33 +236,29 @@ public class BillingSystemTest {
      * Input file should be a txt file,  if not throw an exception
      *
      * @throws RuntimeException
-     *
      */
     @Test
     public void inputFileExtensionNotTxtThrowException() {
-         Path p = Paths.get("D:\\Personnel\\Ingéniance\\Entretien\\Céline\\CandidateInputExample.txt");
+        Path p = Paths.get("D:\\Personnel\\Ingéniance\\Entretien\\Céline\\CandidateInputExample.txt");
         inputFileExtensionNotTxtThrowExceptionImpl(p.getFileName().toString().trim());
 
 
     }
+
     /**
      * Implementation of the Test case 8
      * Input file should be a txt file,  if not throw an exception
      *
      * @throws RuntimeException
-     *
      */
-    public void inputFileExtensionNotTxtThrowExceptionImpl(String fileName ) {
-         Assertions.assertEquals(true ,fileName.endsWith(".txt"));
+    public void inputFileExtensionNotTxtThrowExceptionImpl(String fileName) {
+        Assertions.assertEquals(true, fileName.endsWith(".txt"));
     }
 
 
     /**
      * Test case 9
      * Input file should exist,  if not throw an exception
-     *
-     *
-     *
      */
     @Test
     public void inputFileNotExistThrowException() {
@@ -274,17 +267,14 @@ public class BillingSystemTest {
 
 
     }
+
     /**
      * Implementation of the Test case 9
      * Input file should exist,  if not throw an exception
-     *
-     *
-     *
      */
-    public void inputFileNotExistThrowExceptionImpl(Path p ) {
-        Assertions.assertEquals(true , Files.exists(p));
+    public void inputFileNotExistThrowExceptionImpl(Path p) {
+        Assertions.assertEquals(true, Files.exists(p));
     }
-
 
 
     /**
@@ -292,7 +282,6 @@ public class BillingSystemTest {
      * If the unixTimestamp in the imput file is not defined, throw exception
      *
      * @throws RuntimeException
-     *
      */
     @Test
     public void unixTimestampIsNegativeOrEqualsToZeroThrowException() {
@@ -301,16 +290,16 @@ public class BillingSystemTest {
 
 
     }
+
     /**
      * Implementation of the Test case 10
      * If the unixTimestamp in the imput file is not defined, throw exception
      *
      * @throws RuntimeException
-     *
      */
     public void unixTimestampIsNegativeOrEqualsToZeroThrowExceptionImpl(Journey journey) {
         //Assertions.assertEquals(0 , journey.getUnixTimestamp());
-        if(journey.getUnixTimestamp()==0)
+        if (journey.getUnixTimestamp() == 0)
             throw new RuntimeException("Invalid UnixTimestamp ");
     }
 
@@ -320,7 +309,6 @@ public class BillingSystemTest {
      * If the customerId is not defined or is not exist, throw exception
      *
      * @throws RuntimeException
-     *
      */
     @Test
     public void customerIdIsNotExistThrowException() {
@@ -329,18 +317,18 @@ public class BillingSystemTest {
 
 
     }
+
     /**
      * Implementation of the Test case 11
      * If the customerId is not defined or is not exist, throw exception
      *
      * @throws RuntimeException
-     *
      */
     public void customerIdIsNotExistThrowExceptionImpl(int customerId) {
 
-       Assertions.assertEquals(0,
-               customerList.stream()
-                       .filter(s -> s.getId()==customerId).count());
+        Assertions.assertEquals(0,
+                customerList.stream()
+                        .filter(s -> s.getId() == customerId).count());
     }
 
     /**
@@ -348,7 +336,6 @@ public class BillingSystemTest {
      * If the station is not defined or is not exist , throw exception
      *
      * @throws RuntimeException
-     *
      */
     @Test
     public void stationIsNotExistThrowException() {
@@ -357,16 +344,55 @@ public class BillingSystemTest {
 
 
     }
+
     /**
      * Implementation of the Test case 12
      * If the station is not defined or is not exist , throw exception
      *
      * @throws RuntimeException
-     *
      */
     public void stationIsNotExistThrowExceptionImpl(String stationName) {
 
-      if (stationList.stream().filter(s->s.getName().equalsIgnoreCase(stationName)).collect(Collectors.toList()).size()==0)
-          throw  new RuntimeException("Station not defined");
+        if (stationList.stream().filter(s -> s.getName().equalsIgnoreCase(stationName)).collect(Collectors.toList()).size() == 0)
+            throw new RuntimeException("Station not defined");
     }
+
+    /**
+     * Test case 13
+     * In the journey file each "unixTimestamp"  should correspond to  "customerId" and the "station"
+     * otherwise, invalid file data
+     *
+     * @throws RuntimeException
+     */
+    @Test
+    public void fileDataIsInvalidThrowException() {
+
+        Path p = Paths.get("D:\\Personnel\\Ingéniance\\Entretien\\Céline\\CandidateInputExample.txt");
+        try (Stream<String> fileStream = Files.lines(p)) {
+
+            List<String> tripInformationsFromFile = fileStream.filter(a ->
+                    a.contains("unixTimestamp") ||
+                            a.contains("customerId") ||
+                            a.contains("station")).collect(Collectors.toList());
+
+            fileDataIsInvalidThrowExceptionImpl(tripInformationsFromFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Implementation of the Test case 13
+     * In the journey file each "unixTimestamp"  should correspond to  "customerId" and the "station"
+     *
+     * @throws RuntimeException
+     */
+    public void fileDataIsInvalidThrowExceptionImpl(List<String> tripInformationsFromFile) {
+
+        int unixTimestampNumber = tripInformationsFromFile.stream().filter(a -> a.contains("unixTimestamp")).collect(Collectors.toList()).size();
+        int customerIdNumber = tripInformationsFromFile.stream().filter(a -> a.contains("customerId")).collect(Collectors.toList()).size();
+        int stationNumber = tripInformationsFromFile.stream().filter(a -> a.contains("station")).collect(Collectors.toList()).size();
+        Assertions.assertEquals(true, unixTimestampNumber == customerIdNumber && customerIdNumber == stationNumber);
+    }
+
 }
